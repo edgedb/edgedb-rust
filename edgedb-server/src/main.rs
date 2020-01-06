d62a73105628352cc9b3570de1fcb915e7773bf4
Paul Colomiets <paul@edgedb.com>
2020-01-06 03:54:01 +0200

    server: Add command-line arguments (except --no flags)

--- edgedb-server/src/main.rs
+++ edgedb-server/src/main.rs
@@ -1,2 +1,9 @@
+use structopt::StructOpt;
+
+mod options;
+
+use options::Options;
+
 fn main() {
+    let _options = Options::from_args();
 }
---
+++ edgedb-server/src/options.rs
@@ -0,0 +1,83 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+use structopt::clap::AppSettings;
+
+
+#[derive(StructOpt)]
+#[structopt(setting=AppSettings::UnifiedHelpMessage)]
+pub struct Options {
+    /// Database cluster directory
+    #[structopt(long)]
+    pub data_dir: Option<PathBuf>,
+
+    /// DSN of a remote Postgres cluster, if using one")]
+    #[structopt(short="P", long, value_name="dsn")]
+    pub postgres_dsn: Option<String>,
+
+    /// Logging level. Possible values:
+    /// (d)ebug, (i)nfo, (w)arn, (e)rror, (s)ilent
+    #[structopt(short="l", long)]
+    pub log_level: Option<String>, // TODO(tailhook) make enum
+
+    /// Send logs to <dest>, where <dest> can be a file
+    /// name, "syslog", or "stderr"
+    #[structopt(long, value_name="dest")]
+    pub log_to: Option<PathBuf>,
+
+    /// Bootstrap the database cluster and exit
+    #[structopt(long)]
+    pub bootstrap: bool,
+
+    /// The name of the default database to create
+    #[structopt(long, value_name="text")]
+    pub default_database: Option<String>,
+
+    /// The name of the default database owner
+    #[structopt(long, value_name="text")]
+    pub default_database_user: Option<String>,
+
+    /// enable the development mode
+    #[structopt(long)]
+    pub devmode: bool,
+
+    #[structopt(long, overrides_with="devmode", hidden=true)]
+    pub _no_devmode: bool,
+
+    /// enable the test mode
+    #[structopt(long)]
+    pub testmode: bool,
+
+    #[structopt(long, overrides_with="devmode", hidden=true)]
+    pub _no_testmode: bool,
+
+    /// IP address to listen on
+    #[structopt(short="I", long, value_name="host-or-ip")]
+    pub bind_address: Option<String>,
+
+    /// Port to listen on
+    #[structopt(short="p", long, value_name="integer", default_value="5656")]
+    pub port: u16,
+
+    /// Daemonize
+    #[structopt(short="b", long)]
+    pub background: bool,
+
+    /// Path to PID file directory
+    #[structopt(long, value_name="dir")]
+    pub pidfile: Option<PathBuf>,
+
+    #[structopt(long)]
+    pub daemon_user: Option<u32>,
+    #[structopt(long)]
+    pub daemon_group: Option<u32>,
+
+    /// Directory where UNIX sockets will be created
+    /// ("/run" on Linux by default)
+    #[structopt(long, value_name="dir")]
+    pub runstate_dir: Option<String>,
+
+    #[structopt(long, value_name="int")]
+    pub max_backend_connections: Option<u32>,
+}
+
