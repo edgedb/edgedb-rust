eaec56e7f932bb94ede5c14dfb0640d064dee093
Paul Colomiets <paul@colomiets.name>
2021-09-22 15:30:25 +0300

    edgedb-client: Warn and skip `EDGEDB_PORT=tcp://..`

--- edgedb-client/examples/list_databases.rs
+++ edgedb-client/examples/list_databases.rs
@@ -2,6 +2,8 @@ use std::error::Error;
 use async_std::task;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
     let databases: Vec<String> = task::block_on(async {
         let pool = edgedb_client::connect().await?;
         pool.query("SELECT name := sys::Database.name", &()).await
