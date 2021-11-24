d1506e1d6b2598400dfe0157aeb4eced458171f4
Fantix King <fantix.king@gmail.com>
2021-11-24 10:48:33 -0500

    Fix export

--- edgedb-client/src/errors.rs
+++ edgedb-client/src/errors.rs
@@ -1,2 +1,3 @@
 //! Errors that can be returned by a client
 pub use edgedb_errors::{Error, Tag, ErrorKind, ResultExt, kinds::*};
+pub use edgedb_errors::display::*;
--- edgedb-errors/src/lib.rs
+++ edgedb-errors/src/lib.rs
@@ -1,10 +1,9 @@
-mod display;
 mod error;
 mod traits;
 
+pub mod display;
 pub mod kinds;
 
-pub use display::{display_error, display_error_verbose};
 pub use traits::{ErrorKind, ResultExt};
 pub use error::{Error, Tag};
 pub use kinds::*;
