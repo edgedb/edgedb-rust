2f8420caa329ecffb9d1f7c326b3f4eaf1361e50
Paul Colomiets <paul@colomiets.name>
2023-02-01 17:15:56 +0200

    derive: Implement Queryable for enums
    Fixes #195

---
+++ edgedb-derive/tests/enums.rs
@@ -0,0 +1,18 @@
+use edgedb_derive::Queryable;
+use edgedb_protocol::queryable::{Queryable, Decoder};
+
+#[derive(Queryable, Debug, PartialEq)]
+enum Status {
+    Open,
+    Closed,
+    Invalid,
+}
+
+#[test]
+fn enumeration() {
+    let dec = Decoder::default();
+    assert_eq!(Status::decode(&dec, &b"Open"[..]).unwrap(), Status::Open);
+    assert_eq!(Status::decode(&dec, &b"Closed"[..]).unwrap(), Status::Closed);
+    assert_eq!(Status::decode(&dec, &b"Invalid"[..]).unwrap(), Status::Invalid);
+    Status::decode(&dec, &b"closed"[..]).unwrap_err();
+}
