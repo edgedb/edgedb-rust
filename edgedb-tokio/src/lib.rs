b017965f0cdb428bced1eac0efa1c00afe5ed3d3
Ferdia McKeogh <ferdia@mckeogh.tech>
2023-05-20 14:12:18 +0100

    Export `ClientSecurity`

--- edgedb-tokio/src/lib.rs
+++ edgedb-tokio/src/lib.rs
@@ -56,7 +56,7 @@ mod transaction;
 
 pub use edgedb_derive::{Queryable, GlobalsDelta, ConfigDelta};
 
-pub use builder::{Builder, Config, InstanceName};
+pub use builder::{Builder, Config, InstanceName, ClientSecurity};
 pub use credentials::TlsSecurity;
 pub use client::Client;
 pub use errors::Error;
