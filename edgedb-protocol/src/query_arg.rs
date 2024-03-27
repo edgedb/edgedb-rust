2ff429c2f7eae7ec0b7a559b62935013ac25f1a2
mrfoxpro <tuningiposadka@gmail.com>
2024-03-28 01:56:02 +0500

    feat(named-args): convert identation to spaces

--- edgedb-protocol/src/query_arg.rs
+++ edgedb-protocol/src/query_arg.rs
@@ -533,75 +533,75 @@ implement_tuple! {12, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, }
 pub struct ValueWithCardinality(Option<Value>, Cardinality);
 
 impl<V: Into<Value>> From<V> for ValueWithCardinality {
-	fn from(value: V) -> Self {
-		ValueWithCardinality(Some(value.into()), Cardinality::One)
-	}
+    fn from(value: V) -> Self {
+        ValueWithCardinality(Some(value.into()), Cardinality::One)
+    }
 }
 
 impl<V: Into<Value>> From<Option<V>> for ValueWithCardinality
     where Value: From<V> 
 {
-	fn from(value: Option<V>) -> Self {
-		ValueWithCardinality(value.map(Value::from), Cardinality::AtMostOne)
-	}
+    fn from(value: Option<V>) -> Self {
+        ValueWithCardinality(value.map(Value::from), Cardinality::AtMostOne)
+    }
 }
 
 impl<V: Into<Value>> From<Vec<V>> for ValueWithCardinality
     where Value: From<V>
 {
-	fn from(value: Vec<V>) -> Self {
-		ValueWithCardinality(
-			Some(Value::Array(value.into_iter().map(Value::from).collect())),
-			Cardinality::One
-		)
-	}
+    fn from(value: Vec<V>) -> Self {
+        ValueWithCardinality(
+            Some(Value::Array(value.into_iter().map(Value::from).collect())),
+            Cardinality::One
+        )
+    }
 }
 
 impl<V: Into<Value>> From<Option<Vec<V>>> for ValueWithCardinality
     where Value: From<V>
 {
-	fn from(value: Option<Vec<V>>) -> Self {
-		let mapped = value.map(|value| Value::Array(value.into_iter().map(Value::from).collect()));
-		ValueWithCardinality(mapped, Cardinality::AtMostOne)
-	}
+    fn from(value: Option<Vec<V>>) -> Self {
+        let mapped = value.map(|value| Value::Array(value.into_iter().map(Value::from).collect()));
+        ValueWithCardinality(mapped, Cardinality::AtMostOne)
+    }
 }
 
 pub fn value_from_pairs<K>(pairs: &HashMap<K, ValueWithCardinality>) -> Value
 where
-	K: ToString,
+    K: ToString,
 {
-	let mut elements = Vec::new();
-	let mut fields: Vec<Option<Value>> = Vec::new();
-	for (key, arg) in pairs.iter() {
-		let ValueWithCardinality(value, cd) = arg;
+    let mut elements = Vec::new();
+    let mut fields: Vec<Option<Value>> = Vec::new();
+    for (key, arg) in pairs.iter() {
+        let ValueWithCardinality(value, cd) = arg;
 
-		elements.push(ShapeElement {
-			name: key.to_string(),
-			cardinality: Some(*cd),
-			flag_link: false,
-			flag_link_property: false,
-			flag_implicit: false
-		});
-		fields.push(value.clone());
-	}
+        elements.push(ShapeElement {
+            name: key.to_string(),
+            cardinality: Some(*cd),
+            flag_link: false,
+            flag_link_property: false,
+            flag_implicit: false
+        });
+        fields.push(value.clone());
+    }
 
-	Value::Object {
-		shape: ObjectShape::new(elements),
-		fields
-	}
+    Value::Object {
+        shape: ObjectShape::new(elements),
+        fields
+    }
 }
 
 use std::collections::HashMap;
 impl<K: ToString> From<&HashMap<K, ValueWithCardinality>> for Value
 {
-	fn from(value: &HashMap<K, ValueWithCardinality>) -> Self {
+    fn from(value: &HashMap<K, ValueWithCardinality>) -> Self {
         value_from_pairs(value)
-	}
+    }
 }
 
 impl<K> QueryArgs for HashMap<K, ValueWithCardinality>
 where
-	K: ToString + Send + Sync
+    K: ToString + Send + Sync
 {
     fn encode(&self, encoder: &mut Encoder) -> Result<(), Error> {
         Value::from(self).encode(encoder)
