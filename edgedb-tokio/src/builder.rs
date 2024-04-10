99fb23013760b750ec4f9d4e0f8d76edd026f5a1
Aljaž Mur Eržen <aljaz@edgedb.com>
2024-04-10 09:52:50 +0200

    Fix credentials reading
    When we read database="edgedb" from crednetials.json, we now assume
    that branch="__default__" and not "edgedb".
    
    Behavior before was breaking when we linked a new instance without
    specifing a database or branch. It would write just "edgedb" into crednetials.

--- edgedb-tokio/src/builder.rs
+++ edgedb-tokio/src/builder.rs
@@ -1745,13 +1745,21 @@ impl Config {
                     be saved as credentials file"));
             }
         };
+
+        let branch = Some(&self.0.branch)
+            .filter(|x| x.as_str() != "__default__");
+
         Ok(Credentials {
             host: Some(host.clone()),
             port: *port,
             user: self.0.user.clone(),
             password: self.0.password.clone(),
-            database: if self.0.branch == "__default__" { Some(self.0.database.clone()) } else { None },
-            branch: if self.0.branch == "__default__" { None } else { Some(self.0.branch.clone()) },
+            branch: branch.cloned(),
+
+            // this is not strictly needed (it gets overwritten when reading),
+            // but we want to keep backward compatibility. If you downgrade CLI,
+            // we want it to be able to interact with the new format of credentials.
+            database: branch.cloned(),
             tls_ca: self.0.pem_certificates.clone(),
             tls_security: self.0.tls_security,
             file_outdated: false,
