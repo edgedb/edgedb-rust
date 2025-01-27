fff07bc7351492f1536cc907eb98aa4d8f4c160f
Aljaž Mur Eržen <aljaz@edgedb.com>
2025-01-27 20:23:50 +0100

    Docs for model::Duration

--- gel-protocol/src/model/time.rs
+++ gel-protocol/src/model/time.rs
@@ -7,6 +7,21 @@ use std::time::{SystemTime, UNIX_EPOCH};
 /// A span of time.
 ///
 /// Precision: microseconds.
+/// 
+/// ```
+/// # use gel_protocol::model::Duration;
+/// assert_eq!(&Duration::from_micros(1_000_000).to_string(), "0:00:1");
+/// ```
+/// 
+/// This type implements [FromStr] and parses microseconds or PostgreSQL
+/// simple duration format:
+/// 
+/// ```
+/// # use gel_protocol::model::Duration;
+/// # use std::str::FromStr;
+/// assert_eq!(&Duration::from_str("10987654320").to_string(), "0:00:7.65432");
+/// assert_eq!(&Duration::from_str("2:12:7.65432").to_string(), "2:12:7.65432");
+/// ```
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 #[cfg_attr(feature = "with-serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Duration {
